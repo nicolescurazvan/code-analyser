e86eb87436e9cb1b8148ba59ba12ff12d01a6bb9
Razvan Nicolescu <nicolescurazvan2002@gmail.com>
2025-08-21 19:48:24 +0200

    Example added

--- analyser.py
+++ analyser.py
@@ -1,105 +1,113 @@
 import tkinter as tk
+import time
+import os
 
-# Creating the window
-root = tk.Tk()
-root.title("Code Analyser")
-root["background"] = "#303030"
-
-# Creating the intro label
-intro_label = tk.Label(root, text = "Code Analyser. \n Type in the tests you want to do")
-intro_label["background"] = "#303030"
-intro_label["foreground"] = "#FFFFFF"
-intro_label["font"] = ("Noto Sans", 25)
-intro_label["padx"] = 40
-intro_label["pady"] = 30
-intro_label.grid(row = 0, column = 0, columnspan = 2)
-
-# Creating the frame for the options
-options_frame = tk.LabelFrame(root, text = "Options")
-options_frame["background"] = "#303030"
-options_frame["foreground"] = "#FFFFFF"
-options_frame["font"] = ("Noto Sans", 14)
-options_frame["padx"] = 40
-options_frame["pady"] = 30
-options_frame.grid(row = 1, column = 0, columnspan = 2)
-
-class item:
-    def __init__(self, master, name):
-        self.box = tk.LabelFrame(master, text = name, padx = 20, pady = 20)
-        self.box["background"] = "#505050"
-        self.box["foreground"] = "#FFFFFF"
-        self.box["font"] = ("Noto Sans", 14)
-
-        self.program_var = tk.StringVar()
-        self.args_var = tk.StringVar()
-        self.input_var = tk.StringVar()
-        self.output_var = tk.StringVar()
-        self.ref_var = tk.StringVar()
-        self.check = tk.BooleanVar()
+class Item(tk.LabelFrame):
+    def __init__(self, parent, name):
+        super().__init__(master = parent)
+        self.type = name
+        self["padx"] = self["pady"] = 5
+        self["text"] = name
+        self["bg"] = "#404040"
+        self["fg"] = "#FFFFFF"
         
-        self.program_entry = tk.Entry(self.box, textvariable = self.program_var,
-                                      font = ("Noto Sans", 14), bg = "#505050", fg = "#FFFFFF")
-        self.args_entry = tk.Entry(self.box, textvariable = self.args_var, 
-                                   font = ("Noto Sans", 14), bg = "#505050", fg = "#FFFFFF")
-        self.input_entry = tk.Entry(self.box, textvariable = self.input_var, 
-                                    font = ("Noto Sans", 14), bg = "#505050", fg = "#FFFFFF")
-        self.output_entry = tk.Entry(self.box, textvariable = self.output_var,
-                                      font = ("Noto Sans", 14), bg = "#505050", fg = "#FFFFFF")
-        self.ref_entry = tk.Entry(self.box, textvariable = self.ref_var,
-                                      font = ("Noto Sans", 14), bg = "#505050", fg = "#FFFFFF")
-        self.checkbox = tk.Checkbutton(self.box, text = "Measure", variable = self.check,
-                                        onvalue = True, offvalue = False, font = ("Noto Sans", 14),
-                                        bg = "#505050", fg = "#FFFFFF")
+        # Program input
+        self.program_label = tk.Label(self, text = "Program")
+        self.program_label.grid(row = 0, column = 0)
+        self.program_var = tk.StringVar()
+        self.program = tk.Entry(self, textvariable = self.program_var)
+        self.program.grid(row = 0, column = 1, columnspan = 2)
 
-        self.program_entry.grid(row = 0, column = 0)
-        self.args_entry.grid(row = 0, column = 1)
-        self.input_entry.grid(row = 0, column = 2)
-        self.output_entry.grid(row = 0, column = 3)
-        self.ref_entry.grid(row = 1, column = 1)
-        self.checkbox.grid(row = 1, column = 2)
-        self.box.pack()
+        # Args input
+        self.args_label = tk.Label(self, text = "Args")
+        self.args_label.grid(row = 1, column = 0)
+        self.args_var = tk.StringVar()
+        self.args = tk.Entry(self, textvariable = self.args_var)
+        self.args.grid(row = 1, column = 1, columnspan = 2)
 
-    def get_command(self):
-        command = dict()
-        command["sys"] = self.program_var.get()
-        command["sys"] += " " + self.args_var.get()
-        command["sys"] += " < " + self.input_var.get()
-        command["sys"] += " > " + self.output_var.get()
-        command["sys"] += " 2>> " + "temp.log"
-        command["ref"] = self.ref_var.get()
-        command["measure"] = self.check.get()
-        return command
-    
-items = []
-number = 1
-def add_item():
-    global number
-    items.append(item(options_frame, f"Item {number}"))
-    number += 1
+        # Input file input
+        self.input_label = tk.Label(self, text = "Input file")
+        self.input_label.grid(row = 2, column = 0)
+        self.input_var = tk.StringVar()
+        self.input = tk.Entry(self, textvariable = self.input_var)
+        self.input.grid(row = 2, column = 1, columnspan = 2)
+
+        # Ref file input
+        self.ref_label = tk.Label(self, text = "Reference file")
+        if self.type == "Checker":
+            self.ref_label.grid(row = 3, column = 0)
+        self.ref_var = tk.StringVar()
+        self.ref = tk.Entry(self, textvariable = self.ref_var)
+        if self.type == "Checker":
+            self.ref.grid(row = 3, column = 1, columnspan = 2)
+        
+        self.pack()
+
+    def run(self):
+        exec = "python3 " if self.program_var.get()[-3:] == ".py" else "./"
+        program = self.program_var.get()
+        args = self.args_var.get()
+        input = self.input_var.get()
+        output = "out.txt"
+        if self.type == "Checker":
+            ref = self.ref_var.get()
+            os.system(f"{exec}{program} {args} < {input} > {output} 2>> report.log")
+            os.system(f"diff {output} {ref} >> report.log")
+        else:
+            t = time()
+            os.system(f"{exec}{program} {args} < {input} > {output} 2>> report.log")
+            t = time() - t
+            print(t)
 
 
-# Button to add an item
-add_button = tk.Button(root, text = "+", command = add_item)
-add_button["background"] = "#505050"
-add_button["foreground"] = "#FFFFFF"
-add_button["font"] = ("Noto Sans", 20)
-add_button["padx"] = 40
-add_button["pady"] = 30
-add_button.grid(row = 2, column = 0)
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Code analyser")
+        self.geometry("900x900")
+        self.config(background = "#303030")
+        
+        # Create the intro label
+        self.intro = tk.Label(self)
+        self.intro["text"] = "Press the '+' to insert a command\n" \
+                            "Press the submit button to run the tests\n" \
+                            "Choose the right test for you:\n" \
+                            "1) Checker, to see if the program runs correctly\n" \
+                            "2) Measure, to see how fast a program runs"
+        self.intro["font"] = ("Noto Sans", 15)
+        self.intro["bg"] = "#303030"
+        self.intro["fg"] = "#FFFFFF"
+        self.intro.grid(row = 0, column = 0, columnspan = 3)
 
-# Button to submit
-def submit():
-    for it in items:
-        print(it.get_command())
+        # Create the options frame
+        self.options = tk.Frame(self)
+        self.options["bg"] = "#404040"
+        self.options.grid(row = 1, column = 0, columnspan = 3)
+        self.items = []
 
-submit_button = tk.Button(root, text = "Submit", command = submit)
-submit_button["background"] = "#505050"
-submit_button["foreground"] = "#FFFFFF"
-submit_button["font"] = ("Noto Sans", 20)
-submit_button["padx"] = 40
-submit_button["pady"] = 30
-submit_button.grid(row = 2, column = 1)
+        # Create the buttons
+        self.plus_c = tk.Button(self, text = "New Checker")
+        self.plus_c["font"] = ("Noto Sans", 15)
+        self.plus_c["bg"] = "#303030"
+        self.plus_c["fg"] = "#FFFFFF"
+        self.plus_c["command"] = lambda: self.items.append(Item(self.options, "Checker"))
+        self.plus_c.grid(row = 2, column = 0)
 
-# Display the window
-root.mainloop()
+        self.plus_m = tk.Button(self, text = "New Measure")
+        self.plus_m["font"] = ("Noto Sans", 15)
+        self.plus_m["bg"] = "#303030"
+        self.plus_m["fg"] = "#FFFFFF"
+        self.plus_m["command"] = lambda: self.items.append(Item(self.options, "Measure"))
+        self.plus_m.grid(row = 2, column = 1)
 
+        self.submit = tk.Button(self, text = "Submit")
+        self.submit["font"] = ("Noto Sans", 15)
+        self.submit["bg"] = "#303030"
+        self.submit["fg"] = "#FFFFFF"
+        self.submit["command"] = lambda: print("\n".join([it.run() for it in self.items]))
+        self.submit.grid(row = 2, column = 2)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
